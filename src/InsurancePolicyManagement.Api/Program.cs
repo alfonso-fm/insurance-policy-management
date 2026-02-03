486884054bd4e0d26ad5cb74f9a8cf4eefed0bec
Alfonso Flores Martinez <alfonso.flm@gmail.com>
2026-02-03 11:16:00 -0600

    Enable Swagger in production for demo 3

--- src/InsurancePolicyManagement.Api/Program.cs
+++ src/InsurancePolicyManagement.Api/Program.cs
@@ -126,11 +126,20 @@ if (!app.Environment.IsEnvironment("Testing"))
 #endregion
 
 #region Middleware pipeline
-if (app.Environment.IsDevelopment())
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Insurance API v1");
+    c.RoutePrefix = "swagger";
+});
+
+
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
 
