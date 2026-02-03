eb1a1addc530abfdb419719755ac413d9801b2d3
Alfonso Flores Martinez <alfonso.flm@gmail.com>
2026-02-03 10:01:14 -0600

    Fix: add in-memory configuration for integration tests

--- tests/InsurancePolicyManagement.IntegrationTests/ApiFactory.cs
+++ tests/InsurancePolicyManagement.IntegrationTests/ApiFactory.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using InsurancePolicyManagement.Infrastructure.Persistence.Context;
+using Microsoft.Extensions.Configuration;
 
 namespace InsurancePolicyManagement.IntegrationTests;
 public class ApiFactory : WebApplicationFactory<Program>
@@ -10,6 +11,22 @@ public class ApiFactory : WebApplicationFactory<Program>
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Testing");
+
+        var dictionary = new Dictionary<string, string>
+        {
+            { "Jwt:Key", "TEST_SUPER_SECRET_KEY_1234567890123456" },
+            { "Jwt:Issuer", "TestIssuer" },
+            { "Jwt:Audience", "TestAudience" }
+        };
+
+        // Convert the Dictionary to the expected type
+        IEnumerable<KeyValuePair<string, string?>> initialData = dictionary
+            .Select(kvp => new KeyValuePair<string, string?>(kvp.Key, kvp.Value!)); // Use null-forgiving operator if values are guaranteed non-null
+
+        builder.ConfigureAppConfiguration((context, config) =>{
+            config.AddInMemoryCollection(initialData);
+        });
+
 
         builder.ConfigureServices(services =>
         {
