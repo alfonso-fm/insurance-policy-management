1f5488f361f88f8dac9c5bbbfe530fbaffa3c82d
Alfonso Flores Martinez <alfonso.flm@gmail.com>
2026-02-04 10:58:13 -0600

    Update Jwt Audience

--- src/InsurancePolicyManagement.Api/Program.cs
+++ src/InsurancePolicyManagement.Api/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = true,
-            ValidateAudience = true,
+            ValidateAudience = false,
             ValidateLifetime = true,
             ValidateIssuerSigningKey = true,
             ValidIssuer = builder.Configuration["Jwt:Issuer"],
